d83e767b5578463b42a9715cff87683d8fd262ec
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-11-13 10:23:45 +0100

    Extract bot code into separate class

---
+++ src/bot.py
@@ -0,0 +1,140 @@
+from enum import Enum, auto
+from os import getenv
+
+from dotenv import load_dotenv
+from loguru import logger
+from telegram import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, CommandHandler
+from telegram.ext import ContextTypes as CT
+from telegram.ext import ConversationHandler, Defaults, MessageHandler, PicklePersistence
+from telegram.ext.filters import COMMAND, TEXT, User
+
+from justwatch import JustWatch, MediaEntry
+
+
+class State(Enum):
+    SHOW_DETAILS = auto()
+    SELECT_OFFER_TYPE = auto()
+    SHOW_OFFER = auto()
+
+
+class JustWatchBot:
+    def __init__(self, just_watch: JustWatch) -> None:
+        logger.info("Creating bot...")
+        load_dotenv()
+        self.just_watch = just_watch
+        self.application = (
+            ApplicationBuilder()
+            .token(getenv("TOKEN"))
+            .defaults(Defaults("HTML"))
+            .arbitrary_callback_data(True)
+            .persistence(PicklePersistence(getenv("PERSISTENCE_FILE", "persistence")))
+            .build()
+        )
+        user_filter = User(username=getenv("ALLOWED_USERNAMES", "").split(), allow_empty=True)
+        self.application.add_handler(
+            CommandHandler(["start", "help"], self.help_command, user_filter)
+        )
+        self.application.add_handler(MessageHandler(user_filter & TEXT & ~COMMAND, self.search))
+        self.application.add_handler(self.search_results_handler())
+        logger.info("Bot created!")
+
+    def start(self) -> None:
+        logger.info("Starting bot.")
+        self.application.run_polling()
+
+    async def help_command(self, update: Update, _: CT.DEFAULT_TYPE) -> None:
+        response = "Send a message to the bot with a search query, that's it!"
+        await update.message.reply_text(response)
+
+    def search_results_handler(self) -> ConversationHandler:
+        return ConversationHandler(
+            entry_points=[CallbackQueryHandler(self.show_details_initial)],
+            states={
+                State.SELECT_OFFER_TYPE: [
+                    CallbackQueryHandler(self.search_results_again, "search_results"),
+                    CallbackQueryHandler(self.show_offers),
+                ],
+                State.SHOW_OFFER: [
+                    CallbackQueryHandler(self.show_details_again, "details"),
+                    CallbackQueryHandler(self.search_results_again, "search_results"),
+                ],
+                State.SHOW_DETAILS: [CallbackQueryHandler(self.show_details_initial)],
+            },
+            fallbacks=[],
+            per_message=True,
+            persistent=True,
+            name="search_results_handler",
+        )
+
+    async def search(self, update: Update, context: CT.DEFAULT_TYPE) -> None:
+        search_query = update.message.text.strip()
+        context.user_data["search_query"] = search_query
+        logger.info(f"Looking for '{search_query}'")
+        results = self.just_watch.search(search_query)
+        logger.info(f"Received response for '{search_query}'")
+        context.user_data["search_results"] = results
+        response, keyboard = self.search_response(search_query), self.search_keyboard(results)
+        await update.message.reply_text(response, reply_markup=keyboard)
+
+    async def search_results_again(self, update: Update, context: CT.DEFAULT_TYPE) -> State:
+        query = update.callback_query
+        await query.answer()
+        search_query = context.user_data["search_query"]
+        results = context.user_data["search_results"]
+        response, keyboard = self.search_response(search_query), self.search_keyboard(results)
+        await query.edit_message_text(response, reply_markup=keyboard)
+        return State.SHOW_DETAILS
+
+    def search_response(self, query: str) -> str:
+        return f"Search results for <b>{query}</b>:"
+
+    def search_keyboard(self, data: list[MediaEntry]) -> InlineKeyboardMarkup:
+        buttons = [
+            [InlineKeyboardButton(f"{title} ({year})", callback_data=index)]
+            for index, (title, year, _) in enumerate(data)
+        ]
+        return InlineKeyboardMarkup(buttons)
+
+    async def show_details_initial(self, update: Update, context: CT.DEFAULT_TYPE) -> State:
+        query = update.callback_query
+        await query.answer()
+        index = data if isinstance(data := query.data, int) else 0
+        context.user_data["index"] = index
+        selected_data = context.user_data["search_results"][index]
+        context.user_data["selected_data"] = selected_data
+        return await self.show_details(query, selected_data)
+
+    async def show_details_again(self, update: Update, context: CT.DEFAULT_TYPE) -> State:
+        query = update.callback_query
+        await query.answer()
+        selected_data = context.user_data["selected_data"]
+        return await self.show_details(query, selected_data)
+
+    async def show_details(self, query: CallbackQuery, selected_data: MediaEntry) -> State:
+        title, year, offers = selected_data
+        buttons = [
+            [InlineKeyboardButton(offer_type, callback_data=offer_type)]
+            for offer_type in offers.keys()
+        ]
+        buttons += [[InlineKeyboardButton("« Back", callback_data="search_results")]]
+        keyboard = InlineKeyboardMarkup(buttons)
+        await query.edit_message_text(f"<b>{title}</b> ({year})", reply_markup=keyboard)
+        return State.SELECT_OFFER_TYPE
+
+    async def show_offers(self, update: Update, context: CT.DEFAULT_TYPE) -> State:
+        query = update.callback_query
+        await query.answer()
+        offer_type = query.data
+        title, year, offers = context.user_data["selected_data"]
+        buttons = [
+            [InlineKeyboardButton(f"{name} ({value})", url=url)]
+            for name, url, value in offers[offer_type]
+        ]
+        buttons += [
+            [InlineKeyboardButton("« Back", callback_data="details")],
+            [InlineKeyboardButton("« Back to search", callback_data="search_results")],
+        ]
+        keyboard = InlineKeyboardMarkup(buttons)
+        await query.edit_message_text(f"<b>{title}</b> ({year})", reply_markup=keyboard)
+        return State.SHOW_OFFER
--- src/main.py
+++ src/main.py
@@ -1,154 +1,17 @@
-from enum import Enum, auto
 from os import getenv
-from typing import Any
 
 from dotenv import load_dotenv
-from loguru import logger
-from requests import get
-from telegram import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import (
-    ApplicationBuilder,
-    CallbackQueryHandler,
-    CommandHandler,
-    ContextTypes,
-    ConversationHandler,
-    Defaults,
-    MessageHandler,
-    PicklePersistence,
-)
-from telegram.ext.filters import COMMAND, TEXT, User
 
-load_dotenv()
-TOKEN = getenv("TOKEN")
-API_URL = getenv("API_URL")
-PERSISTENCE_FILE = getenv("PERSISTENCE_FILE", "../persistence")
-ALLOWED_USERNAMES = getenv("ALLOWED_USERNAMES", "").split()
+from bot import JustWatchBot
+from justwatch import JustWatch
 
 
-class State(Enum):
-    SHOW_DETAILS = auto()
-    SELECT_OFFER_TYPE = auto()
-    SHOW_OFFER = auto()
-
-
-def run_bot() -> None:
-    logger.info("Creating bot")
-    application = (
-        ApplicationBuilder()
-        .token(TOKEN)
-        .defaults(Defaults("HTML"))
-        .arbitrary_callback_data(True)
-        .persistence(PicklePersistence(PERSISTENCE_FILE))
-        .build()
-    )
-    user_filter = User(username=ALLOWED_USERNAMES, allow_empty=True)
-    application.add_handler(CommandHandler(["start", "help"], help_command, user_filter))
-    application.add_handler(MessageHandler(user_filter & TEXT & ~COMMAND, search))
-    application.add_handler(search_results_handler())
-    logger.info("Starting bot")
-    application.run_polling()
-
-
-async def help_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-    response = "Send a message to the bot with things you want to search in JustWatch, that's it!"
-    await update.message.reply_text(response)
-
-
-def search_results_handler() -> ConversationHandler:
-    return ConversationHandler(
-        entry_points=[CallbackQueryHandler(show_details_initial)],
-        states={
-            State.SELECT_OFFER_TYPE: [
-                CallbackQueryHandler(search_results_again, "search_results"),
-                CallbackQueryHandler(show_offers),
-            ],
-            State.SHOW_OFFER: [
-                CallbackQueryHandler(show_details_again, "details"),
-                CallbackQueryHandler(search_results_again, "search_results"),
-            ],
-            State.SHOW_DETAILS: [CallbackQueryHandler(show_details_initial)],
-        },
-        fallbacks=[],
-        per_message=True,
-        persistent=True,
-        name="search_results_handler",
-    )
-
-
-async def search(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    search_query = update.message.text.strip()
-    context.user_data["search_query"] = search_query
-    logger.info(f"Looking for '{search_query}'")
-    search_results = get(API_URL.replace("{search_field}", search_query)).json()
-    logger.info(f"Received response for '{search_query}'")
-    context.user_data["search_results"] = search_results
-    response, keyboard = response_and_keyboard(search_query, search_results)
-    await update.message.reply_text(response, reply_markup=keyboard)
-
-
-async def search_results_again(update: Update, context: ContextTypes.DEFAULT_TYPE) -> State:
-    query = update.callback_query
-    await query.answer()
-    search_query = context.user_data["search_query"]
-    search_results = context.user_data["search_results"]
-    response, keyboard = response_and_keyboard(search_query, search_results)
-    await query.edit_message_text(response, reply_markup=keyboard)
-    return State.SHOW_DETAILS
-
-
-def response_and_keyboard(query: str, data: list[Any]) -> tuple[str, InlineKeyboardMarkup]:
-    buttons = [
-        [InlineKeyboardButton(f"{title} ({year})", callback_data=index)]
-        for index, (title, year, offers) in enumerate(data)
-    ]
-    return f"Search results for <b>{query}</b>:", InlineKeyboardMarkup(buttons)
-
-
-async def show_details_initial(update: Update, context: ContextTypes.DEFAULT_TYPE) -> State:
-    query = update.callback_query
-    await query.answer()
-    index = data if isinstance(data := query.data, int) else 0
-    context.user_data["index"] = index
-    current_data = context.user_data["search_results"][index]
-    context.user_data["current_data"] = current_data
-    return await show_details(query, current_data)
-
-
-async def show_details_again(update: Update, context: ContextTypes.DEFAULT_TYPE) -> State:
-    query = update.callback_query
-    await query.answer()
-    current_data = context.user_data["current_data"]
-    return await show_details(query, current_data)
-
-
-async def show_details(query: CallbackQuery, current_data: list[Any]) -> State:
-    title, year, offers = current_data
-    buttons = [
-        [InlineKeyboardButton(offer_type, callback_data=offer_type)] for offer_type in offers.keys()
-    ]
-    buttons += [[InlineKeyboardButton("« Back", callback_data="search_results")]]
-    keyboard = InlineKeyboardMarkup(buttons)
-    await query.edit_message_text(f"<b>{title}</b> ({year})", reply_markup=keyboard)
-    return State.SELECT_OFFER_TYPE
-
-
-async def show_offers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> State:
-    query = update.callback_query
-    await query.answer()
-    offer_type = query.data
-    title, year, offers = context.user_data["current_data"]
-    buttons = [
-        [InlineKeyboardButton(f"{name} ({value})", url=url)]
-        for name, url, value in offers[offer_type]
-    ]
-    buttons += [
-        [InlineKeyboardButton("« Back", callback_data="details")],
-        [InlineKeyboardButton("« Back to search", callback_data="search_results")],
-    ]
-    keyboard = InlineKeyboardMarkup(buttons)
-    await query.edit_message_text(f"<b>{title}</b> ({year})", reply_markup=keyboard)
-    return State.SHOW_OFFER
+def main() -> None:
+    load_dotenv()
+    just_watch = JustWatch(getenv("COUNTRY", "US"))
+    bot = JustWatchBot(just_watch)
+    bot.start()
 
 
 if __name__ == "__main__":
-    run_bot()
+    main()
